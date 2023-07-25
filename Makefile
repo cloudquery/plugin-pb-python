49f557495f1225874b5bf0c5d4bb0c8eca4f5469
Herman Schaaf <hermanschaaf@gmail.com>
2023-07-25 12:04:21 +0100

    Add Discovery service

--- Makefile
+++ Makefile
@@ -4,6 +4,11 @@ clone-proto:
 
 gen:
 	cd plugin-pb && git pull && cd ..
+
+	mkdir -p ./protos/cloudquery/plugin_v3
 	cp ./plugin-pb/plugin/v3/*.proto ./protos/cloudquery/plugin_v3/.
 	python -m grpc_tools.protoc -I./protos --python_out=. --pyi_out=. --grpc_python_out=. ./protos/cloudquery/plugin_v3/*.proto
 
+	mkdir -p ./protos/cloudquery/discovery_v1
+	cp ./plugin-pb/discovery/v1/*.proto ./protos/cloudquery/discovery_v1/.
+	python -m grpc_tools.protoc -I./protos --python_out=. --pyi_out=. --grpc_python_out=. ./protos/cloudquery/discovery_v1/*.proto
---
+++ cloudquery/discovery_v1/discovery_pb2.pyi
@@ -0,0 +1,18 @@
+from google.protobuf.internal import containers as _containers
+from google.protobuf import descriptor as _descriptor
+from google.protobuf import message as _message
+from typing import ClassVar as _ClassVar, Iterable as _Iterable, Optional as _Optional
+
+DESCRIPTOR: _descriptor.FileDescriptor
+
+class GetVersions(_message.Message):
+    __slots__ = []
+    class Request(_message.Message):
+        __slots__ = []
+        def __init__(self) -> None: ...
+    class Response(_message.Message):
+        __slots__ = ["versions"]
+        VERSIONS_FIELD_NUMBER: _ClassVar[int]
+        versions: _containers.RepeatedScalarFieldContainer[int]
+        def __init__(self, versions: _Optional[_Iterable[int]] = ...) -> None: ...
+    def __init__(self) -> None: ...
